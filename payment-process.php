f1075a6d1aab34d9d0ec25adac7f27900b8fc5f4
Alvin Bauma <alvinbauma10@gmail.com>
2022-04-20 20:05:35 +0200

    install external libs

--- payment-process.php
+++ payment-process.php
@@ -1,2 +1,15 @@
 <?php
-echo "cool";
+require 'square/square-sdk/autoload.php';
+
+use Square\SquareClient;
+use Square\LocationApi;
+use Square\Environment;
+use Square\Exceptions\ApiException;
+use Square\Http\ApiResponse;
+use Square\Models\ListLocationsResponse;
+
+
+$client = new SquareClient([
+    'accessToken' => "EAAAEFXbLVgWKvm0myOBdu9jMWcSsbMw3BgXSOuanCLsvVnlk53TaLVOHQq7lcnP",
+    'environment' => Environment::SANDBOX,
+]);
